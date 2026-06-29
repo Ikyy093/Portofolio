b8477a6ea768d426712c7092fbea203c83d9d524
Iky093 <682024093@gmail.com>
2026-06-29 16:25:04 +0700

    Fix Railway deployment

--- app.py
+++ app.py
@@ -92,5 +92,8 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    
-    app.run(debug=Config.DEBUG, host='0.0.0.0', port=5000)
+    app.run(
+        debug=Config.DEBUG,
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 5000))
+    )
